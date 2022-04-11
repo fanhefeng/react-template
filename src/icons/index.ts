f5e6f5b7eb596c31992e2b368cf5948cb5ef675b
Drew Flaherty <drewflaherty@gmail.com>
2022-04-11 19:41:08 +1000

    chore: format files

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,4 +2,4 @@ export { ReactComponent as MetaMaskIcon } from "./metamask.svg";
 export { ReactComponent as WalletConnectIcon } from "./walletConnect.svg";
 export { ReactComponent as DiscordIcon } from "./discord.svg";
 export { ReactComponent as TwitterIcon } from "./twitter.svg";
-export {ReactComponent as InstagramIcon} from "./instagram.svg";
+export { ReactComponent as InstagramIcon } from "./instagram.svg";
