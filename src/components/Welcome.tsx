5f3a59e1519b4e244486b451c7f294298c240b4a
Lorenzo Fontoura <lorenzo@nioel.com>
2022-04-12 14:52:03 +1000

    remove empty tags

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,7 +4,6 @@ const Welcome = () => {
       id="Welcome"
       className="gap-3 grid grid-cols-1 lg:grid-cols-4 pt-60 p-5 pb-20"
     >
-      <div></div>
       <div className="col-span-2">
         <div className="md:text-6xl text-4xl text-black text-center">
           Welcome!
@@ -13,4 +12,5 @@ const Welcome = () => {
     </div>
   );
 };
+
 export default Welcome;
