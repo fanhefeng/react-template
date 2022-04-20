2e3d4d39394bd9267f20657df8694271a748ec43
OptimusOpus <taylor@localcoinswap.com>
2022-04-20 16:21:55 +1000

    Be more explicit with defining different return values from swr hook

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,16 +5,18 @@ import useSWR from "swr";
 const Welcome = () => {
   // This is just here as an example of how to use SWR
   // Feel free to remove
-  const { data, error } = useSWR("https://api.coingecko.com/api/v3/ping");
-  const isLoading = !data && !error;
+  const { data: exampleData, error: exampleError } = useSWR(
+    "https://api.coingecko.com/api/v3/ping"
+  );
+  const isLoading = !exampleData && !exampleError;
 
   return (
     <div
       id="Welcome"
       className="grid grid-cols-1 gap-3 p-5 pb-20 lg:grid-cols-4 pt-60"
     >
-      <ErrorMessage error={error} />
-      <div>{isLoading ? <Loader /> : <p>{data?.gecko_says}</p>}</div>
+      <ErrorMessage error={exampleError} />
+      <div>{isLoading ? <Loader /> : <p>{exampleData?.gecko_says}</p>}</div>
       <div className="col-span-2">
         <div className="text-4xl text-center text-black md:text-6xl">
           Welcome!
