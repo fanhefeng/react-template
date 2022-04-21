334b9673de7aee81617b16d5a24f31852a5b6c39
Barry Earsman <barry@earsman.com>
2022-04-21 12:54:01 +1000

    Use standard eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module"
+  },
   plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
     "plugin:@typescript-eslint/recommended",
@@ -8,13 +12,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    jest: true
+    jest: false
   },
-  ignorePatterns: [".eslintrc.js"],
-  rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off"
-  }
+  ignorePatterns: [".eslintrc.js"]
 };
