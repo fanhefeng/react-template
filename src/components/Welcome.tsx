c308b76572aade2e0f1406aacf79aab3d1cdcae7
Lorenzo Fontoura <lorenzo@nioel.com>
2022-08-15 08:57:29 +1000

    add missing divs for grid

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,6 +6,7 @@ const Welcome = () => {
       id="Welcome"
       className="gap-3 grid grid-cols-1 lg:grid-cols-4 pt-60 p-5 pb-20"
     >
+      <div></div>
       <div className="col-span-2">
         <div className="md:text-6xl text-4xl text-black text-center">
           Welcome!
