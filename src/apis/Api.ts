8da8583b8bc1e744765d9eff9aa6098baa3e1532
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-08-29 10:59:36 +1000

    Added API client

---
+++ src/apis/Api.ts
@@ -0,0 +1,5 @@
+import HTTPClient from "./HTTPClient";
+
+export default class Api extends HTTPClient {
+  public example = async () => "Example call";
+}
---
+++ src/providers/Api.tsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+import Api from "../apis/Api";
+
+export const ApiContext = createContext<Api>(null as unknown as Api);
+
+export const ApiProvider: React.FC<{ children: React.ReactNode }> = ({
+  children
+}) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const api = new Api(process.env.VITE_API_URL! as string, true);
+
+  return <ApiContext.Provider value={api}>{children}</ApiContext.Provider>;
+};
