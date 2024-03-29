5c915f323ae26efbe1a321686d9b596d23ef248b
Soul <710790342@qq.com>
2024-03-29 14:32:05 +0800

    Update PageLayout.tsx
    fix Home component layout by tailwind css

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const PageLayout: React.FC<Props> = ({ children }) => (
-  <div className="w-screen">{children}</div>
+  <div className="w-screen h-screen grid grid-flow-row auto-rows-auto">{children}</div>
 );
 
 export default PageLayout;
